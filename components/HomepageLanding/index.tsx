03cdd92118b4a2e58f087a02850e4c0281236390
Vikrant Bhat <vik.bhat223@gmail.com>
2021-04-07 02:00:40 +0530

    Homepage Landing data

--- components/HomepageLanding/index.tsx
+++ components/HomepageLanding/index.tsx
@@ -11,47 +11,33 @@ const HomepageLanding = () => {
 				<div className="w-full md:w-1/2">
 					<div className="max-w-lg">
 						<h1 className="text-2xl font-medium tracking-wide text-gray-800 dark:text-white md:text-4xl">
-							Find your premium new glasses exported from US
+							Get Accurate Legal Assistance
 						</h1>
 						<p className="mt-2 text-gray-600 dark:text-gray-300">
-							We work with the best remunated glasses dealers in US to find your new
-							glasses.
+							Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda
+							quisquam beatae nobis ipsa consectetur eum tempore impedit cumque,
+							adipisci quidem tempora consequuntur esse quaerat et fuga enim maiores
+							iusto? Reiciendis.
 						</p>
 						<div className="grid gap-6 mt-8 sm:grid-cols-2">
 							<div className="flex items-center space-x-6 text-gray-800 dark:text-gray-200">
 								<CheckIcon fontSize="small" />
-
-								<span>Premium selection</span>
+								<span>Human Resource Consulting</span>
 							</div>
 
 							<div className="flex items-center space-x-6 text-gray-800 dark:text-gray-200">
 								<CheckIcon fontSize="small" />
-
-								<span>Insurance</span>
+								<span>Legal Advisory</span>
 							</div>
 
 							<div className="flex items-center space-x-6 text-gray-800 dark:text-gray-200">
 								<CheckIcon fontSize="small" />
-
-								<span>All legal documents</span>
+								<span>Legal Due Diligence</span>
 							</div>
 
 							<div className="flex items-center space-x-6 text-gray-800 dark:text-gray-200">
 								<CheckIcon fontSize="small" />
-
-								<span>From US glasses dealers</span>
-							</div>
-
-							<div className="flex items-center space-x-6 text-gray-800 dark:text-gray-200">
-								<CheckIcon fontSize="small" />
-
-								<span>Payment Security</span>
-							</div>
-
-							<div className="flex items-center space-x-6 text-gray-800 dark:text-gray-200">
-								<CheckIcon fontSize="small" />
-
-								<span>Fast shipping (+ Express)</span>
+								<span>MSME & Startup Compliance</span>
 							</div>
 						</div>
 					</div>
@@ -60,7 +46,8 @@ const HomepageLanding = () => {
 				<div className="flex items-center justify-center w-full h-96 md:w-1/2">
 					<img
 						className="object-cover w-full h-full max-w-2xl rounded-md"
-						src="https://images.unsplash.com/photo-1555181126-cf46a03827c0?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80"
+						// src="https://images.unsplash.com/photo-1555181126-cf46a03827c0?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80"
+						src="/images/homepage-landing.jpeg"
 						alt="glasses photo"
 					/>
 				</div>
