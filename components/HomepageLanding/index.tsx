95608f89a52374b2cd1b0996c4ea45840d799ff1
Vikrant Bhat <vik.bhat223@gmail.com>
2021-04-07 00:42:38 +0530

    New UI implemented with tailwind

--- components/HomepageLanding/index.tsx
+++ components/HomepageLanding/index.tsx
@@ -1,39 +1,68 @@
 // STYLES
 import styles from './index.module.css'
 
+// MUI
+import CheckIcon from '@material-ui/icons/Check'
+
 const HomepageLanding = () => {
 	return (
-		<header className={styles.header__container}>
-			<div className="absolute top-44 md:top-64 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-screen text-center">
-				<h1 className="text-2xl md:text-6xl font-bold text-white">
-					Bodhankar & Associates
-				</h1>
-				<p className="mt-4 md:text-lg text-white">Management & Legal Consultants</p>
-				<p className="mt-4 md:text-xs text-white font-light text-justify max-w-lg mx-auto hidden md:block">
-					The firm advises on specific queries and documentation with respect to human
-					resource, labour, organization development, HR strategies, statutory acts,
-					government schemes, student counseling and career guidance.
-				</p>
-			</div>
+		<header className="bg-white dark:bg-gray-800">
+			<div className="container flex flex-col px-6 py-10 mx-auto space-y-6 md:h-128 md:py-16 md:flex-row md:items-center md:space-x-6">
+				<div className="w-full md:w-1/2">
+					<div className="max-w-lg">
+						<h1 className="text-2xl font-medium tracking-wide text-gray-800 dark:text-white md:text-4xl">
+							Find your premium new glasses exported from US
+						</h1>
+						<p className="mt-2 text-gray-600 dark:text-gray-300">
+							We work with the best remunated glasses dealers in US to find your new
+							glasses.
+						</p>
+						<div className="grid gap-6 mt-8 sm:grid-cols-2">
+							<div className="flex items-center space-x-6 text-gray-800 dark:text-gray-200">
+								<CheckIcon fontSize="small" />
 
-			<div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-screen text-center">
-				<button className="bg-yellow-400 uppercase px-4 py-2 rounded-md mt-8 hover:bg-yellow-300 font-semibold">
-					Contact us
-				</button>
-			</div>
+								<span>Premium selection</span>
+							</div>
 
-			<div className="absolute top-3/4 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-screen text-center grid grid-cols-3 divide-x divide-white text-white max-w-2xl mx-auto border-width ">
-				<div>
-					<h2 className="font-bold md:text-lg">20+ Years</h2>
-					<p className="font-light text-xs">of Experience</p>
+							<div className="flex items-center space-x-6 text-gray-800 dark:text-gray-200">
+								<CheckIcon fontSize="small" />
+
+								<span>Insurance</span>
+							</div>
+
+							<div className="flex items-center space-x-6 text-gray-800 dark:text-gray-200">
+								<CheckIcon fontSize="small" />
+
+								<span>All legal documents</span>
+							</div>
+
+							<div className="flex items-center space-x-6 text-gray-800 dark:text-gray-200">
+								<CheckIcon fontSize="small" />
+
+								<span>From US glasses dealers</span>
+							</div>
+
+							<div className="flex items-center space-x-6 text-gray-800 dark:text-gray-200">
+								<CheckIcon fontSize="small" />
+
+								<span>Payment Security</span>
+							</div>
+
+							<div className="flex items-center space-x-6 text-gray-800 dark:text-gray-200">
+								<CheckIcon fontSize="small" />
+
+								<span>Fast shipping (+ Express)</span>
+							</div>
+						</div>
+					</div>
 				</div>
-				<div>
-					<h2 className="font-bold md:text-lg">Expert Team</h2>
-					<p className="font-light text-xs">Ready to help you</p>
-				</div>
-				<div>
-					<h2 className="font-bold md:text-lg">Served 1000+</h2>
-					<p className="font-light text-xs">Happy Clients</p>
+
+				<div className="flex items-center justify-center w-full h-96 md:w-1/2">
+					<img
+						className="object-cover w-full h-full max-w-2xl rounded-md"
+						src="https://images.unsplash.com/photo-1555181126-cf46a03827c0?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80"
+						alt="glasses photo"
+					/>
 				</div>
 			</div>
 		</header>
@@ -41,3 +70,38 @@ const HomepageLanding = () => {
 }
 
 export default HomepageLanding
+
+// <header className={styles.header__container}>
+// 	<div className="absolute top-44 md:top-64 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-screen text-center">
+// 		<h1 className="text-2xl md:text-6xl font-bold text-white">
+// 			Bodhankar & Associates
+// 		</h1>
+// 		<p className="mt-4 md:text-lg text-white">Management & Legal Consultants</p>
+// 		<p className="mt-4 md:text-xs text-white font-light text-justify max-w-lg mx-auto hidden md:block">
+// 			The firm advises on specific queries and documentation with respect to human
+// 			resource, labour, organization development, HR strategies, statutory acts,
+// 			government schemes, student counseling and career guidance.
+// 		</p>
+// 	</div>
+
+// 	<div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-screen text-center">
+// 		<button className="bg-yellow-400 uppercase px-4 py-2 rounded-md mt-8 hover:bg-yellow-300 font-semibold">
+// 			Contact us
+// 		</button>
+// 	</div>
+
+// 	<div className="absolute top-3/4 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-screen text-center grid grid-cols-3 divide-x divide-white text-white max-w-2xl mx-auto border-width ">
+// 		<div>
+// 			<h2 className="font-bold md:text-lg">20+ Years</h2>
+// 			<p className="font-light text-xs">of Experience</p>
+// 		</div>
+// 		<div>
+// 			<h2 className="font-bold md:text-lg">Expert Team</h2>
+// 			<p className="font-light text-xs">Ready to help you</p>
+// 		</div>
+// 		<div>
+// 			<h2 className="font-bold md:text-lg">Served 1000+</h2>
+// 			<p className="font-light text-xs">Happy Clients</p>
+// 		</div>
+// 	</div>
+// </header>
--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -40,81 +40,142 @@ export default function Navbar() {
 	const toggleSideDrawer = () => setShowDrawer(!showDrawer)
 
 	return (
-		<div className={classes.root}>
-			<AppBar
-				position="static"
-				style={{
-					backgroundColor: '#35363a'
-				}}>
-				<Toolbar>
-					<div className="mr-4">
-						<Image src="/logo.jpg" height="40" width="40" />
+		<nav className="border-b dark:border-gray-700">
+			<div className="container px-6 py-4 mx-auto md:flex md:justify-between md:items-center">
+				<div className="flex items-center justify-between">
+					<div>
+						{/* <div
+							className="mr-4 inline self-center"
+							style={{ height: '30px !important' }}>
+							<Image src="/logo.jpg" height="30" width="30" />
+						</div> */}
+
+						<a
+							className="text-xl font-bold text-gray-800 dark:text-white md:text-2xl hover:text-gray-700 dark:hover:text-gray-300"
+							href="#">
+							BODHANKAR & ASSOCIATES
+						</a>
 					</div>
-
-					<p className="flex-1 font-bold text-md  md:text-2xl">BODHANKAR & ASSOCIATES</p>
-
-					{/* NAV LINKS */}
-					<div className="hidden md:block">
-						{navbarLinks.map((l, idx) => (
-							<Link href={l.url} key={idx}>
-								<a className="mx-8 hover:text-yellow-400 font-bold">{l.label}</a>
-							</Link>
-						))}
-
-						<Link href="/contact">
-							<a className="mx-8 hover:text-yellow-400 hover:border-yellow-400 font-bold border-2 px-3 py-1 rounded-3xl">
-								Contact
-							</a>
-						</Link>
+					{/* <!-- Mobile menu button --> */}
+					<div className="md:hidden">
+						<button
+							type="button"
+							className="text-gray-500 dark:text-gray-200 hover:text-gray-600 dark:hover:text-gray-400 focus:outline-none focus:text-gray-600 dark:focus:text-gray-400"
+							aria-label="Toggle menu">
+							<svg viewBox="0 0 24 24" className="w-6 h-6 fill-current">
+								<path d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"></path>
+							</svg>
+						</button>
 					</div>
-
-					{/* HAMBURGER MENU ICON */}
-					<span className="md:hidden">
-						<IconButton
-							edge="start"
-							color="inherit"
-							aria-label="menu"
-							onClick={toggleSideDrawer}
-							style={{
-								padding: 0
-							}}>
-							<MenuIcon />
-						</IconButton>
-					</span>
-				</Toolbar>
-			</AppBar>
-
-			<Drawer open={showDrawer} onClose={toggleSideDrawer}>
-				<div className="w-60">
-					<List>
-						{navbarLinks.map((l, idx) => (
-							<ListItem
-								button
-								key={idx}
-								onClick={() => {
-									router.push(l.url)
-									toggleSideDrawer()
-								}}>
-								<ListItemIcon>{l.icon}</ListItemIcon>
-								<ListItemText primary={l.label} />
-							</ListItem>
-						))}
-						<Divider />
-
-						<ListItem
-							button
-							onClick={() => {
-								router.push('/contact')
-								toggleSideDrawer()
-							}}>
-							<ListItemIcon>
-								<ContactPhoneIcon />
-							</ListItemIcon>
-							<ListItemText primary="Contact" />
-						</ListItem>
-					</List>
 				</div>
-			</Drawer>
-		</div>
+
+				{/* <!-- Mobile Menu open: "block", Menu closed: "hidden" --> */}
+				<div className="items-center md:flex">
+					<div className="flex flex-col mt-4 space-y-8 md:flex-row md:items-center md:mt-0 md:space-y-0 md:space-x-16">
+						<a
+							className="block font-medium text-gray-700 dark:text-gray-200 hover:text-gray-900 dark:hover:text-gray-400 hover:underline"
+							href="#">
+							glasses Search
+						</a>
+						<a
+							className="block font-medium text-gray-700 dark:text-gray-200 hover:text-gray-900 dark:hover:text-gray-400 hover:underline"
+							href="#">
+							How it works!
+						</a>
+						<a
+							className="block font-medium text-gray-700 dark:text-gray-200 hover:text-gray-900 dark:hover:text-gray-400 hover:underline"
+							href="#">
+							Why us?
+						</a>
+						<a
+							className="block font-medium text-gray-700 dark:text-gray-200 hover:text-gray-900 dark:hover:text-gray-400 hover:underline"
+							href="#">
+							Contact
+						</a>
+
+						<button className="flex items-center px-5 py-2 text-sm font-medium tracking-wide text-center text-white capitalize transition-colors duration-200 transform bg-gray-700 rounded-md hover:bg-gray-600 focus:outline-none focus:bg-gray-600">
+							Get in touch
+						</button>
+					</div>
+				</div>
+			</div>
+		</nav>
 	)
 }
+
+// <div className={classes.root}>
+// 	<AppBar
+// 		position="static"
+// 		style={{
+// 			backgroundColor: '#35363a'
+// 		}}>
+// 		<Toolbar>
+// 			<div className="mr-4">
+// 				<Image src="/logo.jpg" height="40" width="40" />
+// 			</div>
+
+// 			<p className="flex-1 font-bold text-md  md:text-2xl">BODHANKAR & ASSOCIATES</p>
+
+// 			{/* NAV LINKS */}
+// 			<div className="hidden md:block">
+// 				{navbarLinks.map((l, idx) => (
+// 					<Link href={l.url} key={idx}>
+// 						<a className="mx-8 hover:text-yellow-400 font-bold">{l.label}</a>
+// 					</Link>
+// 				))}
+
+// 				<Link href="/contact">
+// 					<a className="mx-8 hover:text-yellow-400 hover:border-yellow-400 font-bold border-2 px-3 py-1 rounded-3xl">
+// 						Contact
+// 					</a>
+// 				</Link>
+// 			</div>
+
+// 			{/* HAMBURGER MENU ICON */}
+// 			<span className="md:hidden">
+// 				<IconButton
+// 					edge="start"
+// 					color="inherit"
+// 					aria-label="menu"
+// 					onClick={toggleSideDrawer}
+// 					style={{
+// 						padding: 0
+// 					}}>
+// 					<MenuIcon />
+// 				</IconButton>
+// 			</span>
+// 		</Toolbar>
+// 	</AppBar>
+
+// 	<Drawer open={showDrawer} onClose={toggleSideDrawer}>
+// 		<div className="w-60">
+// 			<List>
+// 				{navbarLinks.map((l, idx) => (
+// 					<ListItem
+// 						button
+// 						key={idx}
+// 						onClick={() => {
+// 							router.push(l.url)
+// 							toggleSideDrawer()
+// 						}}>
+// 						<ListItemIcon>{l.icon}</ListItemIcon>
+// 						<ListItemText primary={l.label} />
+// 					</ListItem>
+// 				))}
+// 				<Divider />
+
+// 				<ListItem
+// 					button
+// 					onClick={() => {
+// 						router.push('/contact')
+// 						toggleSideDrawer()
+// 					}}>
+// 					<ListItemIcon>
+// 						<ContactPhoneIcon />
+// 					</ListItemIcon>
+// 					<ListItemText primary="Contact" />
+// 				</ListItem>
+// 			</List>
+// 		</div>
+// 	</Drawer>
+// </div>
