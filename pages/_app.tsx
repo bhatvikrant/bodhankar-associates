ffd0a9266044bccf748fef86f5f3eaf071e4ca83
Vikrant Bhat <vik.bhat223@gmail.com>
2021-04-06 17:01:57 +0530

    MUI setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,37 @@
-import "bootstrap/dist/css/bootstrap.min.css";
-
+import React from 'react';
 import type { AppProps /*, AppContext */ } from "next/app";
-import NavbarComponent from "../components/Navbar";
+import PropTypes from 'prop-types';
+import Head from 'next/head';
+import { ThemeProvider } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import theme from '../theme';
 
-function MyApp({ Component, pageProps }: AppProps) {
-	return (
-		<>
-			<NavbarComponent />
-			<Component {...pageProps} />
-		</>
-	);
+export default function MyApp({ Component, pageProps }: AppProps) {
+
+  React.useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
+
+  return (
+    <React.Fragment>
+      <Head>
+        <title>My page</title>
+        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+      </Head>
+      <ThemeProvider theme={theme}>
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </React.Fragment>
+  );
 }
 
-export default MyApp;
+MyApp.propTypes = {
+  Component: PropTypes.elementType.isRequired,
+  pageProps: PropTypes.object.isRequired,
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,59 +1,12 @@
-import HomepageLanding from "../components/HomepageLanding/index";
 
 const Home = () => {
 	return (
 		<div>
-			<HomepageLanding />
-			Nostrud ullamco sit proident minim consequat consequat occaecat. Elit
-			laboris laboris et adipisicing in culpa officia fugiat pariatur mollit
-			commodo. Aliquip ad laborum ad laboris culpa cillum ex. Aliqua
-			exercitation proident aliquip fugiat incididunt. Et irure id ad incididunt
-			culpa amet velit exercitation. Eiusmod eu duis labore deserunt quis
-			eiusmod esse id duis anim. Elit deserunt enim cupidatat et laboris. Labore
-			aliqua minim veniam incididunt aliqua excepteur incididunt aliquip veniam
-			duis eiusmod. Sunt laboris est laboris ipsum velit do cillum velit.
-			Proident tempor laborum aliquip amet in eiusmod esse. Mollit eiusmod duis
-			cillum amet eu nulla pariatur minim elit. Sunt sit laborum eiusmod non.
-			Est laboris excepteur ex reprehenderit qui aliquip occaecat aliqua laborum
-			excepteur sint. Ullamco amet veniam aliquip aliqua. Aliqua culpa cupidatat
-			qui aliquip consequat sit. Labore fugiat laborum cupidatat esse quis
-			dolore amet aliqua magna anim non. Ut amet cupidatat ad pariatur id
-			exercitation sit cillum pariatur nisi nisi aliquip ad. Adipisicing id
-			proident ea nulla sint ipsum. Ad quis aute labore non occaecat excepteur.
-			Velit ullamco ad enim in. Nostrud officia do pariatur veniam tempor quis
-			Lorem consectetur sint et nisi labore dolore. Cupidatat enim sint ut amet
-			nostrud mollit nisi deserunt reprehenderit fugiat sit ea quis. Eiusmod
-			labore consequat non aliquip nostrud. Et adipisicing minim quis commodo
-			duis exercitation ut velit proident amet nulla nostrud. Nisi ex amet
-			cillum quis ut quis consequat. Proident incididunt nulla nisi fugiat
-			mollit culpa amet aute duis officia adipisicing laboris. Nisi officia
-			dolore nostrud in do irure sit. Veniam anim minim sint do deserunt aliquip
-			dolore minim esse. Sunt eu velit eu nulla nulla quis proident labore nisi.
-			Ullamco nisi cillum eiusmod quis excepteur dolor elit occaecat veniam
-			eiusmod mollit labore adipisicing. Occaecat Lorem nisi aliquip sit. Nisi
-			proident ex duis eu et do Lorem qui ipsum sit ipsum incididunt. Qui enim
-			ut ut mollit enim aliquip. Ut laboris Lorem magna pariatur et. Elit dolor
-			eu exercitation aliqua ipsum Lorem. Ad enim magna quis magna. Ut sint
-			nostrud ad deserunt culpa nisi veniam fugiat ad amet reprehenderit ut et.
-			Voluptate occaecat proident amet eiusmod. Est amet deserunt minim
-			reprehenderit fugiat elit duis ullamco. Est eiusmod amet id voluptate quis
-			duis aliquip nulla. Non aliquip qui est voluptate do sit sint fugiat
-			incididunt esse anim qui. Sit excepteur minim ut proident ut nisi nisi.
-			Anim elit elit deserunt qui qui id minim. Sit laborum incididunt sunt
-			ullamco dolor veniam amet amet cupidatat Lorem ex anim minim. Cupidatat eu
-			enim veniam laboris nulla veniam aute incididunt. Lorem est exercitation
-			sunt qui laborum. Laborum ea laborum ea voluptate mollit. Sint dolore ea
-			labore elit irure irure commodo. Eu incididunt dolor et do dolore mollit
-			sunt amet est. Aliquip aliquip enim exercitation enim cupidatat et.
-			Deserunt mollit culpa occaecat cillum culpa eiusmod velit irure irure.
-			Deserunt pariatur magna est consequat nostrud veniam veniam eiusmod nisi
-			labore cupidatat velit velit. Aute qui velit ea fugiat ullamco. Aliquip
-			qui ullamco sit aute aliqua Lorem commodo ex mollit nostrud Lorem veniam
-			ea. Amet duis dolor sint esse eiusmod ullamco consectetur elit amet
-			tempor. Laborum duis consequat ut commodo anim irure exercitation culpa
-			irure id non adipisicing.
+			home
 		</div>
-	);
+
+
+	)
 };
 
 export default Home;
