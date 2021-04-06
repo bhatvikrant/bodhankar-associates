6f925a0674bdd0d2caff8b52e9aa0393ac24f06b
Vikrant Bhat <vik.bhat223@gmail.com>
2021-04-07 01:00:18 +0530

    Advisory board

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,13 @@
 import HomepageLanding from '../components/HomepageLanding'
 import Trustedby from '../components/Trustedby'
+import AdvisoryBoard from '../components/AdvisoryBoard'
 
 const Home = () => {
 	return (
 		<div>
 			<HomepageLanding />
 			<Trustedby />
+			<AdvisoryBoard />
 		</div>
 	)
 }
