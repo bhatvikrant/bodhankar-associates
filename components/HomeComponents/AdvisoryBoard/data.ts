0b9fbfa22f64e15113c8a597bed0f98bdee591a8
Vikrant Bhat <vik.bhat223@gmail.com>
2021-05-10 01:43:38 +0530

    updated ADVISORY BOARD

---
+++ components/HomeComponents/AdvisoryBoard/data.ts
@@ -0,0 +1,28 @@
+interface IAdvisoryBoard {
+    name: string
+    designation: string
+    description: string
+}
+
+export const advisoryBoard: Array<IAdvisoryBoard> = [
+    {
+        name: 'Dr. (Prof) H.G. Kulkarni',
+        designation: 'BAMS, LLB, LLM, NET, SET, Ph.D (Law), BA Hon. Sr. Consultant; Former Dean',
+        description: 'Faculty of Law (Shivaji University) Former Principal – Law Colleges (Sinhgad & Dayanand), Contributory Faculty on Legal Studies'
+    },
+    {
+        name: 'Mr. Sukhanand Joshi',
+        designation: 'BE, PGDIE, MMS, FIE, FIV, FIIIE Senior Consultant',
+        description: 'Lead Auditor, Chartered Engineer & Valuer & Rotarian'
+    },
+    {
+        name: 'Dr. (Prof) Roshan Kazi',
+        designation: 'MMS, MPhil, Ph.D',
+        description: 'Prof & Head – Management Inst. Program, Member of Board of Studies (Management)'
+    },
+    {
+        name: 'Mr. A. G. Bodhankar',
+        designation: 'BA, LLB Retired Income Tax Officer, community worker',
+        description: 'Vast experience in corporate tax matters, audits, and inspections'
+    },
+]
--- components/HomeComponents/AdvisoryBoard/index.tsx
+++ components/HomeComponents/AdvisoryBoard/index.tsx
@@ -1,11 +1,7 @@
-// STYLES
-import styles from './index.module.css'
+// DATA
+import { advisoryBoard } from "./data"
 
-// TS INTERFACES
-interface Props {}
-
-const AdvisoryBoard = (props: Props): JSX.Element => {
-	const {} = props
+const AdvisoryBoard = (): JSX.Element => {
 
 	return (
 		<section className="text-gray-600 body-font bg-white ">
@@ -15,91 +11,30 @@ const AdvisoryBoard = (props: Props): JSX.Element => {
 						ADVISORY BOARD
 					</h1>
 					<p className="lg:w-2/3 mx-auto leading-relaxed text-base">
-						Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify,
-						subway tile poke farm-to-table. Franzen you probably haven't heard of them.
+						We are active partners of evolving HR & Legal ecosystem that improvises with challenging times and workplace. Eminent and Senior Advisers are our guiding force. They guide us on professional fronts on variety of critical subject matters and related issues.
 					</p>
 				</div>
 				<div className="flex flex-wrap -m-4">
-					<div className="p-4 lg:w-1/4 md:w-1/2">
-						<div className="h-full flex flex-col items-center text-center">
-							<img
-								alt="team"
-								className="flex-shrink-0 rounded-lg w-full h-56 object-cover object-center mb-4"
-								src="https://dummyimage.com/200x200"
-							/>
-							<div className="w-full">
-								<h2 className="title-font font-medium text-lg text-gray-900">
-									Alper Kamu
-								</h2>
-								<h3 className="text-gray-500 mb-3">UI Developer</h3>
-								<p className="mb-4">
-									DIY tote bag drinking vinegar cronut adaptogen squid fanny pack
-									vaporware.
-								</p>
-								
+					{advisoryBoard.map((advisor, idx) => (
+						<div className="p-4 lg:w-1/4 md:w-1/2" key={idx}>
+							<div className="h-full flex flex-col items-center text-center" >
+								<img
+									alt="team"
+									className="flex-shrink-0 rounded-lg w-full h-56 object-cover object-center mb-4"
+									src="https://dummyimage.com/200x200"
+								/>
+								<div className="w-full">
+									<h2 className="title-font font-medium text-lg text-gray-900">
+										{advisor.name}
+									</h2>
+									<h3 className="text-gray-500 mb-3">{advisor.designation}</h3>
+									<p className="mb-4 text-justify">
+										{advisor.description}
+									</p>
+								</div>
 							</div>
 						</div>
-					</div>
-					<div className="p-4 lg:w-1/4 md:w-1/2">
-						<div className="h-full flex flex-col items-center text-center">
-							<img
-								alt="team"
-								className="flex-shrink-0 rounded-lg w-full h-56 object-cover object-center mb-4"
-								src="https://dummyimage.com/201x201"
-							/>
-							<div className="w-full">
-								<h2 className="title-font font-medium text-lg text-gray-900">
-									Holden Caulfield
-								</h2>
-								<h3 className="text-gray-500 mb-3">UI Developer</h3>
-								<p className="mb-4">
-									DIY tote bag drinking vinegar cronut adaptogen squid fanny pack
-									vaporware.
-								</p>
-								
-							</div>
-						</div>
-					</div>
-					<div className="p-4 lg:w-1/4 md:w-1/2">
-						<div className="h-full flex flex-col items-center text-center">
-							<img
-								alt="team"
-								className="flex-shrink-0 rounded-lg w-full h-56 object-cover object-center mb-4"
-								src="https://dummyimage.com/202x202"
-							/>
-							<div className="w-full">
-								<h2 className="title-font font-medium text-lg text-gray-900">
-									Atticus Finch
-								</h2>
-								<h3 className="text-gray-500 mb-3">UI Developer</h3>
-								<p className="mb-4">
-									DIY tote bag drinking vinegar cronut adaptogen squid fanny pack
-									vaporware.
-								</p>
-								
-							</div>
-						</div>
-					</div>
-					<div className="p-4 lg:w-1/4 md:w-1/2">
-						<div className="h-full flex flex-col items-center text-center">
-							<img
-								alt="team"
-								className="flex-shrink-0 rounded-lg w-full h-56 object-cover object-center mb-4"
-								src="https://dummyimage.com/203x203"
-							/>
-							<div className="w-full">
-								<h2 className="title-font font-medium text-lg text-gray-900">
-									Henry Letham
-								</h2>
-								<h3 className="text-gray-500 mb-3">UI Developer</h3>
-								<p className="mb-4">
-									DIY tote bag drinking vinegar cronut adaptogen squid fanny pack
-									vaporware.
-								</p>
-								
-							</div>
-						</div>
-					</div>
+					))}
 				</div>
 			</div>
 		</section>
